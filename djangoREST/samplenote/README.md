8b3efbb0550d138bb3e398587f2790b5e3408e55
alexaoh <alex.andunge@gmail.com>
2019-12-29 17:46:02 +0100

    Added djangoREST-project samplenote (djangoREST-api)

---
+++ djangoREST/samplenote/README.md
@@ -0,0 +1,2 @@
+### Following article: https://dev.to/apcelent/how-to-create-rest-api-using-django-rest-framework-28em
+
---
+++ djangoREST/samplenote/note/models.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+
+from django.db import models
+from django.utils import timezone
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+class Note(models.Model):
+
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=400)
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.CharField(max_length=50,blank=True, null=True)
+    priority = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    
